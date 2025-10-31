1b6ad02a14802a52ddfa59efbd6b31abd6a37103
Kriticos <kriticos@gmail.com>
2025-10-31 10:17:17 -0300

    feat: create db.mssql.psm1

---
+++ modules/db.mssql/db.mssql.psm1
@@ -0,0 +1,14 @@
+<#
+.SYNOPSIS
+    Módulo para interagir com o banco de dados MSSQL.
+
+.DESCRIPTION
+    Este módulo fornece funções para conexão, execução de consultas
+    e manipulação de dados em bancos de dados MSSQL. Pode ser usado
+    em conjunto com outros módulos de coleta de eventos.
+
+.NOTES
+    Autor: Kriticos
+    Data: 2024-09-10
+    Versão: 1.12.0
+#>
