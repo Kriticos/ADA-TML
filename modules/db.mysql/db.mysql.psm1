bfe8bb81021e51894105381e647720daf4d5c47c
Kriticos <kriticos@gmail.com>
2025-10-31 10:17:27 -0300

    feat: create db.mysql.psm1

---
+++ modules/db.mysql/db.mysql.psm1
@@ -0,0 +1,14 @@
+<#
+.SYNOPSIS
+    Módulo para interagir com o banco de dados MySQL.
+
+.DESCRIPTION
+    Este módulo fornece funções para conexão, execução de consultas
+    e manipulação de dados em bancos de dados MySQL. Pode ser usado
+    em conjunto com outros módulos de coleta de eventos.
+
+.NOTES
+    Autor: Kriticos
+    Data: 2024-09-10
+    Versão: 1.12.0
+#>
